82f9db129eb3c38b25c2a0687ec0b91599fa7ee1
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2024-11-06 12:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function moveFocus(currentInput) {
+    let nextInput = document.getElementById(`otp-${currentInput + 1}`);
+    if (nextInput) {
+        nextInput.focus();
+    }
+}
+
+// Move focus to the previous input when backspace is pressed
+function moveFocusBack(event, currentInput) {
+    if (event.key === 'Backspace') {
+        let prevInput = document.getElementById(`otp-${currentInput - 1}`);
+        if (prevInput) {
+            prevInput.focus();
+        }
+    }
+}
