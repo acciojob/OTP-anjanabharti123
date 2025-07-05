deabedd383f5e95192e80cffe01937fb6cb023bf
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2025-07-05 21:13:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 function moveFocus(index) {
-    const input = document.getElementById(`otp-${index}`);
+    const input = document.getElementById(`code-${index}`);
     const value = input.value;
 
     if (!/^\d$/.test(value)) {
-        input.value = ''; // Only allow digits
+        input.value = '';
         return;
     }
 
     if (index < 6) {
-        const nextInput = document.getElementById(`otp-${index + 1}`);
+        const nextInput = document.getElementById(`code-${index + 1}`);
         nextInput.focus();
     }
 }
 
 function moveFocusBack(event, index) {
-    const input = document.getElementById(`otp-${index}`);
+    const input = document.getElementById(`code-${index}`);
 
     if (event.key === "Backspace") {
         if (input.value === '') {
             if (index > 1) {
-                const prevInput = document.getElementById(`otp-${index - 1}`);
+                const prevInput = document.getElementById(`code-${index - 1}`);
                 prevInput.focus();
-                prevInput.value = ''; // Also delete the value in the previous field
+                prevInput.value = '';
             }
         } else {
             input.value = '';
         }
 
-        // Prevent default backspace behavior (like navigating away)
         event.preventDefault();
     }
 }
